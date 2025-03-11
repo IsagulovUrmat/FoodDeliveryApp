db6d5ede009e6ff1e74bd290517030ad5e0a9c91
isagulovurmat <falone641234@gmail.com>
2025-03-11 17:27:33 +0600

    added new base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  AppColor.swift
+//  FoodDeliveryApp
+//
+//  Created by sunflow on 11/3/25.
+//
+
+import UIKit
+
+enum AppColor {
+    static let accentOrange = UIColor(hexString: "#D35400")
+    static let gray = UIColor(hexString: "#ECF0F1")
+    static let black = UIColor(hexString: "#000000")
+    static let background = UIColor(hexString: "#E5E5E5")
+}
